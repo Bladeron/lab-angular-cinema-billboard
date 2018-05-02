0f112c0e3567f5bf3db5caa7be8d47b3a5c83272
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-05-02 19:34:15 +0200

    Iteracion 3 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
@@ -14,6 +14,8 @@ const routes: Routes = [
   { path: 'movie/:id', component: MyMovieComponentComponent }
 ];
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +25,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- starter-code/src/app/my-home-component/my-home-component.component.ts
+++ starter-code/src/app/my-home-component/my-home-component.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from "../movies.service";
 
 @Component({
   selector: 'app-my-home-component',
   templateUrl: './my-home-component.component.html',
-  styleUrls: ['./my-home-component.component.css']
+  styleUrls: ['./my-home-component.component.css'],
+  providers: [MoviesService]
 })
 export class MyHomeComponentComponent implements OnInit {
+  
+  newMovies: Array<Object> = [];
 
-  constructor() { }
+  constructor(private movies: MoviesService) { }
 
   ngOnInit() {
+    this.newMovies = this.movies.getMovies();
   }
 
 }
