d185d82719988595a05ed8eb4d28b32507e56835
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-05-03 09:08:22 +0200

    Ya trae peliculas individuales

--- starter-code/src/app/my-movie-component/my-movie-component.component.ts
+++ starter-code/src/app/my-movie-component/my-movie-component.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from "../movies.service";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-movie-component',
   templateUrl: './my-movie-component.component.html',
-  styleUrls: ['./my-movie-component.component.css']
+  styleUrls: ['./my-movie-component.component.css'],
+  providers: [MoviesService]
 })
 export class MyMovieComponentComponent implements OnInit {
 
-  constructor() { }
+  oneMovie: Object = {};
+  movieId: number;
+
+  constructor(private movies: MoviesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params) => this.movieId = Number(params['id']));
+      
+      this.oneMovie = this.movies.getMovie(this.movieId)
   }
 
 }
